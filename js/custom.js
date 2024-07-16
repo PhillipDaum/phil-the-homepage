282ee2dd9051f2e945b73981a76b6c7790714f29
PhillipDaum <philgdaum@gmail.com>
2024-07-16 14:31:42 -0600

    building out

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,8 @@
 import projects from "../data/data.js"
 
-console.log("get it")
-console.log(projects)
-
-//displays projeccts as cards
+//displays projects as cards
 function displayCards() {
-    console.log("yo")
     for (let item of projects) {
-        console.log("HeLL0000")
         let makeCard = document.createElement("div");
         makeCard.className = "card";
         makeCard.innerHTML = `
@@ -15,7 +10,6 @@ function displayCards() {
         <img src=${item.image} alt="${item.imageAlt}">
         <p>${item.description}</p>
         `
-        // is this right?
         document.querySelector(".card-wrapper").appendChild(makeCard);
     }
 }
