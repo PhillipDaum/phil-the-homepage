5d5f4e3cdf65083d350ed03994de1e3033c65d22
PhillipDaum <philgdaum@gmail.com>
2024-07-17 13:55:06 -0600

    add hamburger menu, footer

--- data/data.js
+++ data/data.js
@@ -5,7 +5,7 @@ let projects = [
         image: "./img/portfolio-coffee.png",
         imageAlt: "",
         description: "Cannons Coffee was my first major project. Starting with images of the site at large and medium dimensions, I coded the HTML and CSS required to create two pages, using CSS Grid for layouts. Later, as I learned JavaScript, I added a hamburger menu for mobile dimensions.",
-        link: "link to site",
+        link: "https://www.notion.so/Phil-s-Dashboard-45638ea7ee7e431abfdba28e0707d205",
     },
     {
 
@@ -13,7 +13,7 @@ let projects = [
         image: "./img/portfolio-icecream.png",
         imageAlt: "",
         description: "In the Ice Cream Ordering Form, I learned HTML form elements, CSS styling, and submitting the form using JavaScript processing. The form incorporates text elements, text area, radio buttons, checkboxes, and a select dropdown.",
-        link: "link to site",
+        link: "https://www.notion.so/Phil-s-Dashboard-45638ea7ee7e431abfdba28e0707d205",
     },
     {
 
@@ -21,7 +21,7 @@ let projects = [
         image: "/img/portfolio-fm.png",
         imageAlt: "",
         description: "Using lessons learned from the Cannons Coffee assignment, I built this website according to specifications provided. I then added some JavaScript elements, including a hamburger button",
-        link: "link to site",
+        link: "https://www.notion.so/Phil-s-Dashboard-45638ea7ee7e431abfdba28e0707d205",
     }
 ]
 export default projects;
--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,15 @@ function displayCards() {
     for (let item of projects) {
         let makeCard = document.createElement("div");
         makeCard.className = "card";
+        // can add link to both the repo and the hosted site
         makeCard.innerHTML = `
         <h4>${item.title}</h4>
         <img src=${item.image} alt="${item.imageAlt}">
         <p>${item.description}</p>
+        <div class="socials">
+          <a href="${item.gitHub}"><i class="fa-brands fa-square-github"></i></a>
+          <a href="${item.gitHubPages}"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+        </div>
         `
         document.querySelector(".card-wrapper").appendChild(makeCard);
     }
@@ -16,7 +21,9 @@ function displayCards() {
 displayCards()
 
 // shows and hides hamburger menu on mobile
-const burger = document.querySelector(".burger i");
+// the event listner way seems to work better then adding onclick in html when using modules in JS
+document.querySelector(".burger-button").addEventListener("click", showHamburger);
+const burger = document.querySelector(".burger-button i");
 const nav = document.querySelector(".nav");
 function showHamburger() {
   burger.classList.toggle("fa-times");
