828c11d2f18f856740e1647dc05026402b42136f
Phil <philgdaum@gmail.com>
2024-07-23 19:10:32 -0600

    add comment

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,11 @@
-// buttons show and hide respective sections
+// variables for each area that shows/hides
 const madlibArea = document.querySelector(".madlib-wrapper");
 const timeArea = document.querySelector(".time-wrapper");
 const colorArea = document.querySelector(".color-wrapper");
 const madlibCard = document.querySelector(".madlib-card");
 const fillerIcon = document.querySelector(".placeholder");
 
+// shows/hides madlib
 const madlibButton = document.getElementById("madlib-button");
 madlibButton.addEventListener("click", showMeMadlib);
 function showMeMadlib() {
@@ -21,6 +22,7 @@ function showMeMadlib() {
   }
   madlibArea.classList.toggle("show-me");
 }
+// shows/hides time area
 const timeButton = document.getElementById("time-button");
 timeButton.addEventListener("click", showTimeSection);
 function showTimeSection() {
@@ -37,6 +39,7 @@ function showTimeSection() {
   }
   timeArea.classList.toggle("show-me");
 }
+// shows/hides color area
 const colorButton = document.getElementById("color-button");
 colorButton.addEventListener("click", showMeColor);
 function showMeColor() {
@@ -54,14 +57,13 @@ function showMeColor() {
   colorArea.classList.toggle("show-me");
 }
 
+// submits madlib form and shows created madlib card area
 const madlibForm = document.getElementById("madlib-form");
 madlibForm.addEventListener("submit", onMadLibFormSubmit);
-// on madlib form submit....
 function onMadLibFormSubmit(event) {
   event.preventDefault();
   const data = new FormData(event.target);
   const dataObject = Object.fromEntries(data.entries());
-
   const madlib = `
   in a ${dataObject.adjective1} tech utopia, a coder named ${dataObject.humanName} always wore their ${dataObject.adjective2} hat and sipped ${dataObject.noun1}. One day, they discovered a ${dataObject.noun2} that could ${dataObject.verb1} anything!
   With this power, ${dataObject.humanName} started ${dataObject.verbEndingInIng} all the ${dataObject.pluralNoun1}, quickly becoming the "${dataObject.adjective3} Coder." Then, a ${dataObject.adjective4} ${dataObject.animal} warned them about the harmful ${dataObject.villainName}. Grabbing their trusty ${dataObject.noun3}, ${dataObject.humanName} set off to defeat ${dataObject.villainName}.
@@ -70,11 +72,10 @@ function onMadLibFormSubmit(event) {
   madlibCard.classList.toggle("show-me");
   madlibArea.classList.toggle("show-me");
   document.querySelector(".madlib-story").textContent = madlib;
-  // can make overall UX for javascript area cleaner
   madlibForm.reset();
 }
 
-// world time stuffs
+// submits world time form and updates clock
 const timeForm = document.getElementById("time-form");
 timeForm.addEventListener("submit", onTimeFormSubmit);
 function onTimeFormSubmit(e) {
@@ -83,15 +84,12 @@ function onTimeFormSubmit(e) {
   const dataObject = Object.fromEntries(data.entries());
   let date = new Date();
   let displayTime = date.toLocaleString('en-us', dataObject).split(",")[1];
-  console.log(displayTime);
-  // logs time for the chosen area
   document.querySelector(".clock-face").textContent = displayTime;
-  console.log(dataObject);
 }
 
-// changing colors
-// more radio buttons that change the theme!
-// have the form element thing.... then
+// submits form for color theme, changes colors
+// seems to only change colors on this page
+// NEED TO CHANGE THE COLORS IT MAKES IT ALL BLACK NOW!
 const colorForm = document.getElementById("color-form");
 colorForm.addEventListener("submit", onColorFormSubmit);
 function onColorFormSubmit(e) {
