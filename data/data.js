a10333ab13192d6dd2d6f340f6647b08289cf78b
Phil <philgdaum@gmail.com>
2024-07-24 16:59:24 -0600

    styling

--- data/data.js
+++ data/data.js
@@ -1,9 +1,7 @@
-// ADD horoscopes, art page, city picker
+// WORK on descriptions
 // ADD image alt text
-
 let projects = [
     {
-
         title: "Cannon's Coffee",
         image: "./img/portfolio-coffee.png",
         imageAlt: "",
@@ -12,7 +10,6 @@ let projects = [
         gitHubPages: "https://phillipdaum.github.io/cannons-coffee/",
     },
     {
-
         title: "Ice Cream",
         image: "./img/portfolio-icecream.png",
         imageAlt: "",
@@ -21,7 +18,6 @@ let projects = [
         gitHubPages: "https://phillipdaum.github.io/cannons-coffee/",
     },
     {
-
         title: "Farmer's Market",
         image: "/img/portfolio-fm.png",
         imageAlt: "",
@@ -29,35 +25,31 @@ let projects = [
         gitHub: "https://github.com/PhillipDaum/cannons-coffee",
         gitHubPages: "https://phillipdaum.github.io/cannons-coffee/",
     },
-    // Change these
-    // art page, horoscopes, city picker, anything else?
-    // get pictures for these! 
+    // FIX PICS FOR THESE
+    // ADD image alt
     {
-
-        title: "Cannon's Coffee",
+        title: "City Picker",
         image: "./img/portfolio-coffee.png",
         imageAlt: "",
-        description: "Cannons Coffee was my first major project. Starting with images of the site at large and medium dimensions, I coded the HTML and CSS required to create two pages, using CSS Grid for layouts. Later, as I learned JavaScript, I added a hamburger menu for mobile dimensions.",
-        gitHub: "https://github.com/PhillipDaum/cannons-coffee",
-        gitHubPages: "https://phillipdaum.github.io/cannons-coffee/",
+        description: "After learning loops, I returned to this previously unfinished project and breathed life into it! Use this to find cities where it is likely to be sunny.",
+        gitHub: "https://github.com/PhillipDaum/city-picker",
+        gitHubPages: "https://phillipdaum.github.io/city-picker/",
     },
     {
-
-        title: "Ice Cream",
+        title: "PhilDaum.art",
         image: "./img/portfolio-icecream.png",
         imageAlt: "",
-        description: "In the Ice Cream Ordering Form, I learned HTML form elements, CSS styling, and submitting the form using JavaScript processing. The form incorporates text elements, text area, radio buttons, checkboxes, and a select dropdown.",
-        gitHub: "https://github.com/PhillipDaum/",
-        gitHubPages: "https://phillipdaum.github.io/cannons-coffee/",
+        description: "I built a new art portfolio site for myself after completing a 6-week course in Bootstrap a couple of years ago.",
+        gitHub: "https://github.com/PhillipDaum/phildaum.art",
+        gitHubPages: "https://www.phildaum.art/",
     },
     {
-
-        title: "Farmer's Market",
+        title: "Daily Horoscope",
         image: "/img/portfolio-fm.png",
         imageAlt: "",
-        description: "Using lessons learned from the Cannons Coffee assignment, I built this website according to specifications provided. I then added some JavaScript elements, including a hamburger button",
-        gitHub: "https://github.com/PhillipDaum/cannons-coffee",
-        gitHubPages: "https://phillipdaum.github.io/cannons-coffee/",
+        description: "I built this while learning how to move around data objects!",
+        gitHub: "https://github.com/PhillipDaum/horoscopes",
+        gitHubPages: "https://phillipdaum.github.io/horoscopes/",
     }
 ]
 export default projects;
