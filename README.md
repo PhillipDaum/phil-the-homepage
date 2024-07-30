0df35b8e1d4782264788c07eb23864e38a6b0382
Phil <philgdaum@gmail.com>
2024-07-30 16:53:12 -0600

    add something for local testing

---
+++ README.md
@@ -0,0 +1,33 @@
+# My Portfolio Website
+
+Welcome to my portfolio website repository. This project showcases my work, including various web development projects and a contact form connected to Firebase.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Firebase Configuration](#firebase-configuration)
+- [Contact](#contact)
+- [License](#license)
+
+## Introduction
+
+This is my personal portfolio website where I display my projects and provide a way for visitors to contact me. The site is built using HTML, CSS, and JavaScript, and the contact form is integrated with Firebase for data storage.
+
+## Features
+
+- Responsive design
+- Projects showcase
+- Contact form with Firebase integration
+- Hosted on Vercel
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Firebase
+- Vercel
+
+
--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,18 @@
-// Dope, now I need spam protection!!! lol. 
-// and I don't wanna check the database all the time either....
-// Import the functions you need from the SDKs you need
+// Firebase stuff, now I may need spam protection!!! lol. 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+// uncomment this out for local testing
+// import firebaseConfig from "./config.js";
 
-// HOW DO I ACCESS THESE???
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
